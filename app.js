317aad1d533745fef58c1a601ed8d666f3a60686
RonnyK36 <ronnykelvyne3@gmail.com>
2022-08-18 18:13:42 +0300

    Fixed Tab Content not aligning

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ tabsEl.addEventListener("click", (event) => {
 
     const id = event.target.dataset.id;
     if (id) {
+        articlesEl.forEach((article) => {
+            article.classList.remove("live");
+        });
         btnsEl.forEach((btn) => {
             btn.classList.remove("live");
         });
         event.target.classList.add("live");
-        articlesEl.forEach((article) => {
-            article.classList.remove("live");
-        });
+
 
         const element = document.getElementById(id);
         element.classList.add("live");
